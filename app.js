f26640e6eec1e29d721059653f084176a5da2916
Your Name <KERQ>
2022-10-20 16:16:41 +0300

    Update

---
+++ app.js
@@ -0,0 +1,34 @@
+const firstNumber = document.getElementById
+("first-number");
+const secondNumber = document.getElementById
+("second-number");
+//const equalButton = document.getElementById
+//("equal");
+const carpButton = document.querySelector("#Carp")
+const ToplaButton = document.querySelector("#Topla")
+const CikarButton = document.querySelector("#Cikar")
+const BolButton = document.querySelector("#Bol")
+
+const resultText = document.getElementById
+("sonuc");
+
+carpButton.addEventListener("click" , () => {
+    let x = Number(firstNumber.value);
+    let y = Number(secondNumber.value);
+
+    let sonuc = x*y;
+
+    resultText.innerHTML= `Result: ${sonuc}`;
+});
+
+
+//equalButton.addEventListener("click" , () => {
+//   let x = Number(firstNumber.value);
+//   let y = Number(secondNumber.value);
+
+//     let sonuc = x+ y;
+
+//     resultText.innerHTML= `Result: ${sonuc}`;
+
+
+// });
